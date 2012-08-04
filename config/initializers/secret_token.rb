65058ac39157dd66f0181ef6ee08518f6eb1db26
rawhat <me@rawhat.net>
2012-08-04 18:07:00 -0400

    Initial site creation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RawhatSite::Application.config.secret_token = 'cffaf32543dd9ea9976dfb0ca3a62e7bcdac5ee80aba4690bf2adec30a53c94f7ea370a7c97c6cd1aa416cc5e9f6595e62f1e0da7413dba5028c17e86e29c891'
